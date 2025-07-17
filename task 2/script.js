59d6da8792dd09725d81dddad0a44495a40d0974
Avnish Srivastava <srivastavavicks@gmail.com>
2025-07-17 20:36:42 +0530

    completed assignment 2

--- task 2/script.js
+++ task 2/script.js
@@ -3,12 +3,47 @@ function handelchange(e) {
     const formdata = new FormData(form)
     console.log(formdata)
     let enable = true
+    let strength = -1
+    let regex = /(?:[1-9]|10)/
     let inputs = document.querySelectorAll('input[type="text"], input[type="email"], input[type="password"], input[type="date"]');
     inputs.forEach((ele => {
         if (!ele.value.trim()) {
             enable = false
         }
+        if (ele.type === "password" && ele.name === "password" && ele.value.length > 10 && ele.value.includes("@") && regex.test(ele.value) && ele.value.charCodeAt(0) >= 65 && ele.value.charCodeAt(0) <= 90
+        ) {
+            strength = 2
+        }
+        if (ele.type === "password" && ele.name === "password" && ele.value.length > 10 && !ele.value.includes("@") && regex.test(ele.value) && ele.value.charCodeAt(0) >= 65 && ele.value.charCodeAt(0) <= 90
+        ) {
+            strength = 1
+        }
+        if (ele.type === "password" && ele.name === "password" && ele.value.length <= 10 && !ele.value.includes("@") && !regex.test(ele.value) && !(ele.value.charCodeAt(0) >= 65 && ele.value.charCodeAt(0) <= 90
+        )) {
+            strength = 0
+        }
+        if (ele.type === "password" && ele.name === "password" && ele.value === "") {
+            strength = -1
+        }
     }))
+    let showstrength = document.querySelector("#strength")
+    if (strength === -1) {
+        showstrength.style.color = "white"
+        showstrength.innerHTML = ""
+    }
+    if (strength === 2) {
+        showstrength.style.color = "green"
+        showstrength.innerHTML = "strong"
+    }
+    else if (strength === 1) {
+        showstrength.style.color = "orange"
+        showstrength.innerHTML = "medium"
+    }
+    else if (strength === 0) {
+        showstrength.style.color = "red"
+        showstrength.innerHTML = "weak"
+    }
+
     let select = document.querySelector('select[name="gender"]')
     let check = document.querySelector('.check')
 
@@ -31,7 +66,13 @@ function show(e) {
     localStorage.setItem("userdata", JSON.stringify(obj))
     // res.textContent = JSON.stringify(obj, null, 2);
 }
-
+function checkpassword(e) {
+    let val = document.querySelector('input[name="password"]').value
+    let newval = e.target.value
+    if (val !== newval) {
+        alert("passwords does not match")
+    }
+}
 function fdToObject(fd) {
     const obj = {};
     fd.forEach((value, key) => {
